4eb3c5d63154f42e3549ef25fe45227057165184
Dalai Felinto <dfelinto@gmail.com>
2017-04-08 14:21:19 +0200

    STUBS: Go over //source/blender/ only, and do grep

--- stubs.py
+++ stubs.py
@@ -37,14 +37,19 @@ Output:
 #define DO_NOT_USE_GL_SOURCE2_RGB 0
 """
 
-import sys
+import fnmatch
 import os
+import re
+import sys
 
 
 PREAMBLE = """
 /**
  * List automatically generated from `gl-deprecated.h` and `glew.h`
  */
+ """
+
+ENUMS = """
 
 /**
  * ENUM values
@@ -129,21 +134,64 @@ def get_filepaths(argv):
         'gl_deprecated': gl_deprecated,
         'glew': glew,
         }
+
     return filepaths
+
+
+def all_files_get(base_folder):
+    all_files = []
+
+    includes = ('*.c', '*.cpp', '*.h', '*.m', '*.mm')
+    excludes = (
+        '*.git', '*.svn',
+        'bgl.c',
+        'GPU_legacy_subs.h',
+        'blenderplayer',
+        'extern',
+        'gameengine',
+        'tests',
+        'tools',
+        )
+
+    base_folder = os.path.join(base_folder, 'source', 'blender')
+
+    for root, dirs, files in os.walk(base_folder, topdown=True):
+        # excludes can be done with fnmatch.filter and complementary set,
+        # but it's more annoying to read.
+        dirs[:] = [d for d in dirs if d not in excludes]
+        for pat in includes:
+            for f in fnmatch.filter(files, pat):
+                all_files.append(os.path.join(root, f))
+
+    return all_files
+
+
+def found_in_blender(all_files, word):
+    for f in all_files:
+        for line in open(f, 'r'):
+            if word in line:
+                return True
+    return False
 
 
 def main(argv):
     filepaths = get_filepaths(argv)
 
-    defines = []
-    inputs = []
+    enums = []
+    functions = []
 
     with open(filepaths['gl_deprecated']) as f:
         for line in f:
-            if line.startswith("#define gl"):
-                inputs.append(line[len("#define "):-1].split(' ')[0])
-            elif line.startswith("#define GL_"):
-                defines.append("#define {0} 0".format(line.split(' ')[2][:-1]))
+            parts = line[:-1].split(' ')
+
+            if parts[0] != '#define':
+                continue
+
+            if parts[1].startswith('gl'):
+                functions.append(parts[1])
+
+            elif parts[1].startswith('GL_'):
+                enums.append((parts[1], parts[2]))
 
     lookups = {}
     with open(filepaths['glew'], 'r') as f:
@@ -156,32 +204,51 @@ def main(argv):
                 rest = get_rest(parts)
                 name = parts[3]
 
-                lookups[name] = "{_GL_type} DO_NOT_USE_{rest} {_GL_type}_RET".format(
+                lookups[name] = '{_GL_type} DO_NOT_USE_{rest} {_GL_type}_RET'.format(
                     _GL_type=_GL_type,
                     rest=rest,
                     )
 
-    matches = []
-    mismatches = []
+    # We now check if those functions are even called in Blender
+    # Otherwise we shouldn't bother
+    all_files = all_files_get(filepaths['base_blender'])
+    print("Grepping the enums")
+    blender_enums = {e[1] for e in enums if found_in_blender(all_files, e[0])}
+    print("Grepping the functions")
+    blender_functions = {w for w in functions if found_in_blender(all_files, w)}
 
-    for i in inputs:
-        match = lookups.get(i)
+    blender_funcs_okay = []
+    blender_funcs_error = []
+
+    for f in blender_functions:
+        match = lookups.get(f)
         if match is not None:
-            matches.append(match)
+            blender_funcs_okay.append(match)
         else:
-            mismatches.append(i)
+            blender_funcs_error.append(f)
 
+    print('Writing output')
     with open('stubs.c', 'w') as f:
         f.write(PREAMBLE)
-        f.write('\n'.join(defines))
-        f.write(FUNCTIONS)
-        f.write('\n'.join(matches))
+
+        if blender_enums:
+            f.write(ENUMS)
+            formatted_enums = ('#define {0} 0'.format(e) for e in blender_enums)
+            f.write('\n'.join(formatted_enums))
+
+        if blender_funcs_okay:
+            f.write(FUNCTIONS)
+            f.write('\n'.join(blender_funcs_okay))
+
         f.write(END)
 
     with open('stubs.err', 'w') as f:
-        f.write('\n'.join(mismatches))
+        f.write('\n'.join(blender_funcs_error))
 
-    print("Results written to stubs.*")
+    if blender_funcs_error:
+        print("Results written to stubs.err and stubs.c")
+    else:
+        print("Results written to stubs.c")
 
 
 if __name__ == "__main__":
